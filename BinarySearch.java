410ac027f162762aa9a5d7b7fd3268510a808144
Samir <samir.huseynzade.966@gmail.com>
2023-02-25 20:34:22 +0200

    BinarySearch leet code problem
    Tag: Binary Search

---
+++ BinarySearch.java
@@ -0,0 +1,26 @@
+class BinarySearch {
+    public int search(int[] nums, int target) {
+        if(nums.length == 0 || nums == null) {
+            return -1;
+        }
+
+        int mid = 0;
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left <= right) {
+            mid = left + ((right - left)/2);
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[mid] < target) {
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return -1;
+        
+    }
+}
