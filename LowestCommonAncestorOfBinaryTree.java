9f0cc1bd483ccb4d34c0942bf0a31c8b3b402dc9
Samir <samir.huseynzade.966@gmail.com>
2023-04-18 22:35:42 +0300

    LowestCommonAncestorOfBinaryTree started
    tag: binary tree

---
+++ LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class LowestCommonAncestorOfBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+    }
+}
