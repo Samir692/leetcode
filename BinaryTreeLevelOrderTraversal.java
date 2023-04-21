7843a5b45d73b2f50145e7aeac5b1c6e69b1fbad
Samir <samir.huseynzade.966@gmail.com>
2023-04-21 22:49:08 +0300

    BinaryTreeLevelOrderTraversal completed
    Tag: Tree

--- BinaryTreeLevelOrderTraversal.java
+++ BinaryTreeLevelOrderTraversal.java
@@ -15,6 +15,28 @@
  */
 class BinaryTreeLevelOrderTraversal {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        
+        List<List<Integer>> res = new ArrayList<>();
+
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(root);
+
+        while(!q.isEmpty()){
+            ArrayList<Integer> level = new ArrayList<>();
+
+            int qLength = q.size();
+            for(int i = 0; i < qLength; i++){
+                TreeNode lastPoll = q.poll();
+                if(lastPoll != null){
+                    level.add(lastPoll.val);
+                    q.add(lastPoll.left);
+                    q.add(lastPoll.right);
+                }
+            } 
+            if(level.size() > 0){
+                res.add(level);
+            }
+        }
+        return res;
     }
 }
