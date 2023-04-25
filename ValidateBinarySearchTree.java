45342e2329a0ffce25444d99285b01d0e250c6a8
Samir <samir.huseynzade.966@gmail.com>
2023-04-25 23:02:52 +0300

    ValidateBinarySearchTree completed
    Tag: tree

--- ValidateBinarySearchTree.java
+++ ValidateBinarySearchTree.java
@@ -15,17 +15,20 @@
  */
 class ValidateBinarySearchTree {
     public boolean isValidBST(TreeNode root) {
-        
+        if(root == null) return true;
+        return valid(root, null, null);
     }
 
-    boolean valid(TreeNode node, int left, int right){
-        if(node != null){
-            return true;
-        }
+    boolean valid(TreeNode node, Integer left, Integer right){
+        if(node == null) return true;
         
-        if !(node.val < right && root.right.val > root.val){
+        if (
+            (left != null && node.val <= left) || (right != null && node.val >= right)
+        ){
             return false;
         }
+        return valid(node.left, left, node.val) &&
+        valid(node.right, node.val, right);
 
     }
 }
