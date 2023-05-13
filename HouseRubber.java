de8543bde7e4d257490bcc0b301bb0fa3adeec22
Samir <samir.huseynzade.966@gmail.com>
2023-05-13 22:25:20 +0300

    HouseRubber completed
    Tag: dymanic programming

--- HouseRubber.java
+++ HouseRubber.java
@@ -2,7 +2,12 @@ class HouseRubber {
     public int rob(int[] nums) {
         int rob1 = 0, rob2 = 0;
 
+        for(int n : nums){
+            int temp = Math.max(n+rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
 
-        
+        return rob2;       
     }
 }
